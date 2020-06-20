8a41d85293615057a3f4f1f0feb2c9f0bd0f3ab2
Peter Forret <peter@forret.com>
2020-06-20 20:17:38 +0200

    make script pass "shellcheck"
    correct some legacy code to make the script pass shellcheck

--- configure-skeleton.sh
+++ configure-skeleton.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-script_name=`basename "$0"`
-git_name=`git config user.name`;
-git_email=`git config user.email`;
+script_name=$(basename "$0")
+git_name=$(git config user.name)
+git_email=$(git config user.email)
 
 read -p "Author name ($git_name): " author_name
 author_name=${author_name:-$git_name}
@@ -14,11 +14,12 @@ username_guess=${author_name//[[:blank:]]/}
 read -p "Author username ($username_guess): " author_username
 author_username=${author_username:-$username_guess}
 
-current_directory=`pwd`
-current_directory=`basename $current_directory`
+current_directory=$(pwd)
+current_directory=$(basename "$current_directory")
 read -p "Package name ($current_directory): " package_name
 package_name=${package_name:-$current_directory}
-class_name=`echo "$package_name" | sed 's/[-_]/ /g' | awk '{for(j=1;j<=NF;j++){ $j=toupper(substr($j,1,1)) substr($j,2) }}1' | sed 's/\s//g'`
+# convert my-class-title to MyClassTitle - RODO: use to subsctitute ./src/*
+class_name=$(echo "$package_name" | sed 's/[-_]/ /g' | awk '{for(j=1;j<=NF;j++){ $j=toupper(substr($j,1,1)) substr($j,2) }}1' | sed 's/\s//g')
 
 read -p "Package description: " package_description
 echo
@@ -36,7 +37,7 @@ then
 fi
 
 echo
-files=`egrep -r -l ":author|:package" *  | grep -v $script_name`
+files=$(egrep -r -l ":author|:package" ./*  | grep -v "$script_name")
 
 for file in $files ; do
     echo "Customising file $file"
