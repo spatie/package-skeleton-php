0e33cfc99afbff1dad20204a2974b4bbbd2fdfa1
brendt <brendt@users.noreply.github.com>
2022-12-08 09:13:56 +0000

    Fix styling

--- tests/ExampleTestPest.php
+++ tests/ExampleTestPest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 it('can test', function () {
     expect(true)->toBeTrue();
 });
--- tests/ExampleTestPhpunit.php
+++ tests/ExampleTestPhpunit.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace VendorName\Skeleton\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+final class ExampleTest extends TestCase
 {
 }
